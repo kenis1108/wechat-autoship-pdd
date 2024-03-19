c8534681bb86b28c26e514b691d0c682fd0f18d0
kenis <1836362346@qq.com>
2024-03-19 22:34:07 +0800

    fix: 解析automa.json数据

--- models/tables/orderQuery.ts
+++ models/tables/orderQuery.ts
@@ -2,7 +2,7 @@
  * @Author: kenis 1836362346@qq.com
  * @Date: 2024-03-16 11:48:03
  * @LastEditors: kenis 1836362346@qq.com
- * @LastEditTime: 2024-03-18 20:32:05
+ * @LastEditTime: 2024-03-19 22:31:37
  * @FilePath: \wechat-autoship-pdd\models\tables\orderQuery.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,11 +18,11 @@ export const createOrderQueryTableSql = `
   CREATE TABLE orderQuery (
     id INTEGER PRIMARY KEY,
     orderNum TEXT NOT NULL UNIQUE,  -- 订单编号
-    transactionTime TEXT, -- 成交时间
-    productTitle TEXT,  -- 商品标题
-    sku TEXT,  -- 商品属性
-    address TEXT,  -- 收货地址
-    consignee TEXT,  -- 收件人
+    transactionTime TEXT NOT NULL, -- 成交时间
+    productTitle TEXT NOT NULL,  -- 商品标题
+    sku TEXT NOT NULL,  -- 商品属性
+    address TEXT NOT NULL,  -- 收货地址
+    consignee TEXT NOT NULL,  -- 收件人
     extensionNum TEXT NOT NULL,  -- 分机号
     createdAt TEXT DEFAULT (datetime('now', 'localtime'))
   );
--- src/spider/automa/index.ts
+++ src/spider/automa/index.ts
@@ -2,7 +2,7 @@
  * @Author: kenis 1836362346@qq.com
  * @Date: 2024-03-15 15:20:42
  * @LastEditors: kenis 1836362346@qq.com
- * @LastEditTime: 2024-03-18 20:37:54
+ * @LastEditTime: 2024-03-19 22:29:11
  * @FilePath: \wechat-autoship-pdd\src\spider\automa\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,7 +22,7 @@ import { orderQueryTable, orderQueryTableRow } from "../../../models/tables/orde
 const startAutoma = async (isKMG: boolean = true) => {
   const db = new SQLiteDB('autoship.db');
   /** 第一步：启动keymousego去启动automa下载automa.json */
-  isKMG && exec('pnpm keymousego') && await delay(3 * 60 * 1000);
+  isKMG && exec('pnpm keymousego') && await delay(1 * 60 * 1000);
 
   /** 第二步：解析automa.json获取需要的数据存起来xlsx/database */
   if (isFileExists(AUTOMA_JSON_PATH)) {
@@ -33,14 +33,16 @@ const startAutoma = async (isKMG: boolean = true) => {
     const jsonData: AutomaJson[] = JSON.parse(fileContent);
     const result = jsonData.filter(item => item.details.includes('订单编号')).map(({ details }) => {
       const all = details.trim().split('\n').filter(Boolean).filter(i => i !== '\t')
-      const orderNum = all.filter(i => i.includes('订单编号：'))[0].slice(5)
-      const productTitle = all[all.findIndex(i => i === '发货') + 1]
-      const consignee = all[all.findIndex(i => /^\[\d{4}\]$/.test(i)) - 1]
-      const extensionNum = all[all.findIndex(i => /^\[\d{4}\]$/.test(i))].slice(1, 5)
-      const transactionTime = all.filter(i => i.includes('成交时间：'))[0].slice(5, 21)
-      const sku = all[all.findIndex(i => i === '待发货') - 1]
+      const orderNum = all.filter(i => i.includes('订单编号：'))?.[0]?.slice(5)
+      const productTitle = all.findIndex(i => i === '发货') !== -1 ? all[all.findIndex(i => i === '发货') + 1] : ''
+      const consignee = all.findIndex(i => i === '待发货') !== -1 ? all[all.findIndex(i => i === '待发货') + 4] : ''
+      const indexOfExtensionNum = all.findIndex(i => /^\[\d{4}\]$/.test(i))
+      const extensionNum = indexOfExtensionNum !== -1 ? all[indexOfExtensionNum].slice(1, 5) : ''
+      const transactionTime = all.filter(i => i.includes('成交时间：'))?.[0]?.slice(5, 21)
+      const sku = all.findIndex(i => i === '待发货') ? all[all.findIndex(i => i === '待发货') - 1] : ''
       // TODO: 该正则还需要改进，匹配不了所有情况
-      const address = all[all.findIndex(i => /^([\u4e00-\u9fa5]+省)\s([\u4e00-\u9fa5]+市)\s([\u4e00-\u9fa5]+[市|区|镇])\s.*\[(\d{4})\]$/.test(i))]
+      const indexOfAddress = all.findIndex(i => /^([\u4e00-\u9fa5]+[省|市])\s([\u4e00-\u9fa5]+市)\s([\u4e00-\u9fa5]+[市|区|镇])\s.*$/.test(i))
+      const address = indexOfAddress !== -1 ? all[indexOfAddress] : ''
 
       // 存入数据库
       db.insertOne<orderQueryTableRow>(orderQueryTable, {
