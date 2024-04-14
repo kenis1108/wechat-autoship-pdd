f0a3def8677317bbdee54ce4b084ee428b152577
kenis <1836362346@qq.com>
2024-04-14 13:52:08 +0800

    chore: add start browser script

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@
  * @Author: kenis 1836362346@qq.com
  * @Date: 2024-03-09 11:39:45
  * @LastEditors: kenis 1836362346@qq.com
- * @LastEditTime: 2024-04-11 22:43:49
+ * @LastEditTime: 2024-04-14 13:51:36
  * @FilePath: \wechaty-pdd-auto\src\config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -74,4 +74,4 @@ export const SYMBOLS_FORFFERENT_PRODUCTS = '➕'
  * msedge.exe --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222
  * http://127.0.0.1:9222/json/version
  */
-export const BROWSER_WS_ENDPOINT = 'ws://127.0.0.1:9222/devtools/browser/7860948d-ce0b-4c96-bc74-2e212cd640d2'
+export const BROWSER_WS_ENDPOINT = 'ws://127.0.0.1:9222/devtools/browser/aaa36bb9-fd2d-4fbf-b01e-dbac53fa3704'
---
+++ scripts/start_browser.ps1
@@ -0,0 +1,1 @@
+msedge.exe --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222
