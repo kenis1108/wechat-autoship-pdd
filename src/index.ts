a11bab74cea540a89774b9ba35c494ba1b006537
kenis <1836362346@qq.com>
2024-04-22 16:54:30 +0800

    feat(getBrowserWSEndpoint): 自动获取BROWSER_WS_ENDPOINT

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: kenis 1836362346@qq.com
  * @Date: 2024-03-08 15:33:01
  * @LastEditors: kenis 1836362346@qq.com
- * @LastEditTime: 2024-04-22 16:36:23
+ * @LastEditTime: 2024-04-22 16:54:24
  * @FilePath: \wechaty-pdd-auto\src\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,5 +17,5 @@ import bot from './wechaty';
   // // 启动爬虫
   await startSpider(SPIDER_MODE);
   // // 启动wechaty
-  // await bot.start()
+  await bot.start()
 })()
